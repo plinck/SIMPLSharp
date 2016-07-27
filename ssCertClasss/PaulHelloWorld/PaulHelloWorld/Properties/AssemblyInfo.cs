6d77db544534b1372ef823193f1a631db9c7468a
Paul Linck <paul.linck@gmail.com>
2016-07-27 07:12:28 -0400

    Deleted Obsolete Files
    These files were stubs used at beginning of class and no longer needed.
    They are out of date and dont work as intended.  All work in this is
    rolled into ssCertMain

--- ssCertClasss/PaulHelloWorld/PaulHelloWorld/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("PaulHelloWorld")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PaulHelloWorld")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
--- ssCertClasss/SimplSharpClassTemplate1/SimplSharpClassTemplate1/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SimplSharpClassTemplate1")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimplSharpClassTemplate1")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
